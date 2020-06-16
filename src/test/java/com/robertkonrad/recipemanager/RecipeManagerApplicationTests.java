22922aa9e1adad051af394d53539df9b0010f19f
Robert <rokon12@gmail.com>
2020-06-16 13:36:08 +0200

    first commit

---
+++ src/test/java/com/robertkonrad/recipemanager/RecipeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertkonrad.recipemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
